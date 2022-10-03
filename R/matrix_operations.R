5a0d45b19315b00241b869cd78648841f2e4e45e
jennalandy <jennamlandy@gmail.com>
2022-10-03 12:43:53 -0400

    update notation normalize'

--- R/matrix_operations.R
+++ R/matrix_operations.R
@@ -1,26 +1,16 @@
 #' Normalize a matrix such that columns or rows sum to 1
 #'
 #' @param mat input matrix
-#' @param across one of c('cols','rows')
+#' @param sum one of c('cols','rows')
 #'
 #' @return normalized matrix
 #' @export
-normalizeMat <- function(mat, across = 'cols') {
-  if (across == 'cols') {
-    colsums_mat <- matrix(
-      rep(colSums(mat),dim(mat)[1]),
-      nrow = dim(mat)[1],
-      byrow = TRUE
-    )
-    mat <- mat/colsums_mat
+normalizeMat <- function(mat, sum = 'cols') {
+  if (sum == 'cols') {
+    mat <- multiplyAcross(mat, 1/colSums(mat), across = 'rows')
     return(mat)
-  } else if (across == 'rows') {
-    rowsums_mat <- matrix(
-      rep(rowSums(mat),dim(mat)[2]),
-      nrow = dim(mat)[1],
-      byrow = FALSE
-    )
-    mat <- mat/rowsums_mat
+  } else if (sum == 'rows') {
+    mat <- multiplyAcross(mat, 1/rowSums(mat), across = 'cols')
     return(mat)
   } else {
     stop('`across` must be one of c("cols","rows"')
--- tests/testthat/test-normalizeMat.R
+++ tests/testthat/test-normalizeMat.R
@@ -1,12 +1,12 @@
 test_that("noramlizeMat works", {
   mat <- matrix(runif(2*3), nrow = 2)
 
-  mat_cols1 <- normalizeMat(mat, across = 'cols')
+  mat_cols1 <- normalizeMat(mat, sum = 'cols')
   expect_equal(colSums(mat_cols1), c(1,1,1))
 
 
-  mat_rows1 <- normalizeMat(mat, across = 'rows')
+  mat_rows1 <- normalizeMat(mat, sum = 'rows')
   expect_equal(rowSums(mat_rows1), c(1,1))
 
-  expect_error(normalizeMat(mat, across = 'abc'))
+  expect_error(normalizeMat(mat, sum = 'abc'))
 })
